f70984f3d954b79f182a71ffae64502f39a201fa
Luiz-Bianchi <luiz.bianchi@atmansystems.com>
2022-01-30 12:27:45 -0300

    ChapterII done :white_check_mark:

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { HighlightCard } from "../../components/HighlightCard";
 
-import { 
+import { HighlightCard } from "../../components/HighlightCard";
+import { TransactionCard, TransactionCardProps } from "../../components/TransactionCard";
+
+import {
     Container,
     Header,
     UserInfo,
@@ -11,29 +13,98 @@ import {
     UserName,
     UserWrapper,
     Icon,
-    HighlightCards
+    HighlightCards,
+    Transactions,
+    Title,
+    TransactionList
 } from "./styles";
 
-export function Dashboard(){
-    return(
+export interface DataListProps extends TransactionCardProps {
+    id: string;
+}
+
+export function Dashboard() {
+
+    const data: DataListProps[] = [
+        {
+            id: '1',
+            type: 'positive',
+            title: "Desenvolvimento de Site",
+            amount: "R$ 12.000,00",
+            category: {
+                name: 'vendas',
+                icon: 'dollar-sign'
+            },
+            date: "13/04/2020"
+        },
+        {
+            id: '2',
+            type: 'negative',
+            title: "Hamburgueria Pizzy",
+            amount: "R$ 59,00",
+            category: {
+                name: 'alimentação',
+                icon: 'coffee'
+            },
+            date: "13/04/2020"
+        },
+        {
+            id: '3',
+            type: 'positive',
+            title: "Aluguel do Apartamento",
+            amount: "R$ 1.200,00",
+            category: {
+                name: 'Casa',
+                icon: 'shopping-bag'
+            },
+            date: "13/04/2020"
+        }
+    ]
+
+    return (
         <Container>
             <Header>
                 <UserWrapper>
                     <UserInfo>
-                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/62765965?v=4' }}/ >
+                        <Photo source={{ uri: 'https://avatars.githubusercontent.com/u/62765965?v=4' }} />
                         <User>
                             <UserGreeting>Olá,</UserGreeting>
                             <UserName>Luiz</UserName>
                         </User>
                     </UserInfo>
-                    <Icon name="power"/>    
+                    <Icon name="power" />
                 </UserWrapper>
             </Header>
             <HighlightCards>
-                <HighlightCard/>
-                <HighlightCard/>
-                <HighlightCard/>
+                <HighlightCard
+                    type="up"
+                    title="Entradas"
+                    amount="R$ 17.400,00"
+                    lastTransaction="Última entrada dia 13 de abril"
+                />
+                <HighlightCard
+                    type="down"
+                    title="Saídas"
+                    amount="R$ 1.259,00"
+                    lastTransaction="Última saída dia 03 de abril"
+                />
+                <HighlightCard
+                    type="total"
+                    title="Total"
+                    amount="R$ 16.141,00"
+                    lastTransaction="01 à 16 de abril"
+                />
             </HighlightCards>
+
+            <Transactions>
+                <Title>Listagem</Title>
+
+                <TransactionList
+                    data={data}
+                    keyExtractor={item => item.id}
+                    renderItem={({ item }) => <TransactionCard data={item} />}
+                />
+            </Transactions>
         </Container>
     )
 };
--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components/native';
+import { FlatList } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { RFPercentage, RFValue } from 'react-native-responsive-fontsize'
-import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
+
+import { DataListProps } from './index';
 
 export const UserWrapper = styled.View`
     width: 100%;
@@ -20,7 +23,7 @@ export const Container = styled.View`
 export const Header = styled.View`
     width: 100%;
     height: ${RFPercentage(42)}px;
-    background-color: ${({ theme }) => theme.colors.primary };
+    background-color: ${({ theme }) => theme.colors.primary};
 
     justify-content: center;
     align-items: flex-start;
@@ -49,7 +52,7 @@ export const UserGreeting = styled.Text`
 `;
 
 export const UserName = styled.Text`
-    color: ${({ theme}) => theme.colors.shape};
+    color: ${({ theme }) => theme.colors.shape};
     font-size: ${RFValue(18)}px;
     font-family: ${({ theme }) => theme.fonts.bold};
 `;
@@ -68,3 +71,23 @@ export const HighlightCards = styled.ScrollView.attrs({
     position: absolute;
     margin-top: ${RFPercentage(20)}px;
 `;
+
+export const Transactions = styled.View`
+    flex: 1;
+    padding: 0 24px;
+    margin-top: ${RFPercentage(12)}px;
+`;
+
+export const Title = styled.Text`
+    font-size: ${RFValue(18)}px;
+    font-family: ${({ theme }) => theme.fonts.regular};
+    margin-bottom: 16px;
+`;
+
+export const TransactionList = styled(
+    FlatList as new () => FlatList<DataListProps>).attrs({
+        showsVerticalScrollIndicator: false,
+        contentContainerStyle: {
+            paddingBottom: getBottomSpace()
+        }
+    })``;
